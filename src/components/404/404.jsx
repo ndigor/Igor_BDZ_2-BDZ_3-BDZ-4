028cb04fedd688f51daeb993f776fa6e00001809
ndFrontender <108065561+ndigor@users.noreply.github.com>
2023-05-08 18:43:06 +0300

    Создана 404 страница

---
+++ src/components/404/404.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import svg from './svg.svg';
+import './404.css';
+
+const 404 = (props) => {
+    return (
+        <>
+            <div className="container">
+                <div className="404__wrapper">
+                    <img
+                        className="404__img"
+                        src={svg}
+                        alt="sad smile"
+                    />
+                    <h2 className="404__title">
+                        Извините, данная страничка сломалась либо больше не существует.
+                    </h2>
+                    <button className="404__btn">На главную</button>
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default 404;
