f65910989714e046072b532107e04d98cc25b544
ndFrontender <108065561+ndigor@users.noreply.github.com>
2023-05-08 18:30:30 +0300

    Update Header.jsx

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,17 +3,31 @@ import './header.css';
 import Logo from '../Logo/Logo';
 import Search from './Search/Search';
 import HeaderIcons from './HeaderIcons/HeaderIcons';
-import { useState } from 'react';
+import { Link, useLocation } from 'react-router-dom';
 
 export const Header = (props) => {
-    const [user, setUser] = useState(localStorage.getItem('user12'));
+    const setSearchQuery = (path) => {
+        props.setSearch(path);
+    };
+
+    const location = useLocation();
+
     return (
         <header className="header">
             <div className="container">
                 <div className="header__wrapper">
-                    <Logo />
-                    <Search setSearch={() => {}} />
-                    <HeaderIcons user={user} upd={setUser} />
+                    <Link to={'/'}>
+                        <div className="header__logo">
+                            <Logo />
+                        </div>
+                    </Link>
+                    {location.pathname === '/' ||
+                    location.pathname === '/notfoundProduct' ? (
+                        <Search setSearch={setSearchQuery} />
+                    ) : (
+                        ''
+                    )}
+                    <HeaderIcons />
                 </div>
             </div>
         </header>
