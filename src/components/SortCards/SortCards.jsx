3785e947d1c1d20acd08c91b113be46c5ea97895
ndFrontender <108065561+ndigor@users.noreply.github.com>
2023-05-08 20:32:01 +0300

    Create SortCards.jsx
    Реализация сортировки карточек(фильтрация)

---
+++ src/components/SortCards/SortCards.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import './sortCards.css';
+
+const SortCards = ({ onSort }) => {
+    const sortItem = [
+        { id: 'all', title: 'Все товары' },
+        { id: 'popular', title: 'Популярные' },
+        { id: 'new', title: 'Новинки' },
+        { id: 'lowPrice', title: 'Сначала дешёвые' },
+        { id: 'highPrice', title: 'Сначала дорогие' }.
+        { id: 'sale', title: 'По скидке' },
+    ];
+    return (
+        <div className="sort__cards_wrapper">
+            <div className="sort__cards">
+                {sortItem.map((item) => {
+                    return (
+                        <span
+                            key={item.id}
+                            onClick={() => onSort(item.id)}
+                            className="sort__cards_text"
+                        >
+                            {item.title}
+                        </span>
+                    );
+                })}
+            </div>
+        </div>
+    );
+};
+
+export default SortCards;
