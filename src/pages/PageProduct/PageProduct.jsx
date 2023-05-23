ac4bb24c84325dd8dd687bd34d602ec0ffd707fc
ndFrontender <108065561+ndigor@users.noreply.github.com>
2023-05-23 21:46:28 +0300

    Create PageProduct.jsx

---
+++ src/pages/PageProduct/PageProduct.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ProductView from '../../components/ProductView/ProductView';
+
+const PageProduct = () => {
+    return (
+        <div>
+            <ProductView />
+        </div>
+    );
+};
+
+export default PageProduct;
