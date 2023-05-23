bf7dea991ce993744ebf6764a150df16bb1153da
ndFrontender <108065561+ndigor@users.noreply.github.com>
2023-05-23 21:44:04 +0300

    Create NotFoundProductPage.jsx
    Не найдена страница продукта

---
+++ src/pages/NotFoundProductPage/NotFoundProductPage.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import svg from '../NotFoundPage/svg.svg';
+import './notFoundProductPage.css';
+import { Link, NavLink } from 'react-router-dom';
+
+const NotFoundProductPage = ({ search, setSearch }) => {
+    // console.log({ search });
+    // console.log({ setSearch });
+    return (
+        <>
+            <div className="container">
+                <div className="notFoundProductPage__wrapper">
+                    <img
+                        className="notFoundProductPage__img"
+                        src={svg}
+                        alt="sad smile"
+                    />
+                    <h2 className="notFoundProductPage__title">
+                        Простите, товары по вашему запросу не найдены.
+                    </h2>
+                    <Link to={'/'}>
+                        {/* Вот здесь вопрос: не перекидывает на главную потомцу что не принят и не обнулен search? */}
+                        {/* Как обнулить значение в инпуте ??? */}
+                        <button
+                            className="notFoundProductPage__btn"
+                            onClick={() => {
+                                setSearch('') && <NavLink to="/"></NavLink>;
+                            }}
+                        >
+                            На главную
+                        </button>
+                    </Link>
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default NotFoundProductPage;
